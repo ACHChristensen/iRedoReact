edfc144aa969df442c228095efae54979b2425ad
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2025-11-26 17:01:53 +0100

    made navbar responsive

--- src/components/navbar-top/NavBar.tsx
+++ src/components/navbar-top/NavBar.tsx
@@ -1,5 +1,4 @@
-import { IconButton, Image, Stack } from "@chakra-ui/react";
-
+import { IconButton, Image, Flex, Stack } from "@chakra-ui/react";
 import logoDark from "../../assets/iRedo-logo-darkmode.png";
 import logoLight from "../../assets/iRedo-logo.png";
 import { useColorMode } from "../ui/color-mode";
@@ -7,24 +6,39 @@ import { LuMoon, LuSun } from "react-icons/lu";
 import PlatformSelector from "./PlatformSelector";
 import LoginButton from "../../pages/login/LoginButton";
 
-
 export const NavBar = () => {
-    const { toggleColorMode, colorMode } = useColorMode();
+  const { toggleColorMode, colorMode } = useColorMode();
+
   return (
-  <Stack direction={{ base: "column", md: "row" }} justifyContent="space-between" bgColor={colorMode === "light" ? "white" : "#003B54"} boxShadow="md" paddingStart="2rem" paddingEnd="2rem">
-    <Stack direction={{ md: "row", base: "column" }} h="20" >
-    <Image src={colorMode === "light" ? logoLight : logoDark} className="marked" boxSize="20" width="10pc" alignSelf="center"/>
-        
-    </Stack>
-        <Stack justifyContent="center">
-        <PlatformSelector/>
-        </Stack>
-        <Stack direction={{ base: "column", md: "row" }}>
-          <IconButton onClick={toggleColorMode} variant="ghost" size="sm" boxSize="6" alignSelf="center">
+    <Flex
+      direction={{ base: "column", md: "row" }}
+      align="center"
+      justify="space-between"
+      bgColor={colorMode === "light" ? "white" : "#003B54"}
+      px={{ base: 4, md: 8 }}
+      py={4}
+      boxShadow="md"
+      wrap="wrap"
+      gap={4}
+    >
+      {/* Logo */}
+      <Image
+        src={colorMode === "light" ? logoLight : logoDark}
+        alt="iRedo Logo"
+        boxSize={{ base: 12, md: 20 }}
+        objectFit="contain"
+      />
+
+      {/* Platform menu */}
+      <PlatformSelector />
+
+      {/* Right side: theme toggle + login */}
+      <Stack direction="row" justifyContent="space-between" align="center">
+        <IconButton onClick={toggleColorMode} variant="ghost" size="sm" boxSize="6" alignSelf="center">
             {colorMode === "light" ? <LuSun /> : <LuMoon />}
         </IconButton>
-          <LoginButton/>
-        </Stack>
-    </Stack>
+        <LoginButton />
+      </Stack>
+    </Flex>
   );
 };
