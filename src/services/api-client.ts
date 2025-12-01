fc328d3a63a6fccd236903f22842bf6bd3768b9c
ACHChristensen <49302440+ACHChristensen@users.noreply.github.com>
2025-12-01 17:42:38 +0100

    Change Authorization Bearer token in api-client
    Updated the Authorization header with a new Bearer token.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -24,7 +24,7 @@ const axiosInstance = axios.create({
   baseURL: import.meta.env["VITE_API_URL"], // make sure it's "http://localhost:8080"
   headers: {
     "Content-Type": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTc2NDE2OTg3MywiZXhwIjoxNzY0MTczNDczfQ.CbWOgg0-Zr4AGrzfrg4ZZvVnaROdwh5Kj-cm20L3SHY",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTc2NDYwNzI2MSwiZXhwIjoxNzY0NjEwODYxfQ.nU_Ye32A5UKrBisaxR3XFrN561xYfxzA6NcQqh09_rc",
   },
 });
 
